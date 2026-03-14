8109fe4525a21a27f3752c3e980cd867a13fe7bb
Đặng Huy Phong <huyphong6306@gmail.com>
2026-03-15 00:03:17 +0700

    test nckh4

--- KTX-Management/loginForm.cs
+++ KTX-Management/loginForm.cs
@@ -44,7 +44,7 @@ namespace KTX_Management
                     }
                     else
                     {
-                        MessageBox.Show("failed!");
+                        MessageBox.Show("failedddd!");
                     }
 
                 }
