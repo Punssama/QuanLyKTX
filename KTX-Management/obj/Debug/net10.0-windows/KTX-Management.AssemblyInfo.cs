8bf0415282bccdbe24bdb91d68d72edd1662b80f
Đặng Huy Phong <huyphong6306@gmail.com>
2026-03-15 11:14:28 +0700

    test sonar

--- KTX-Management/obj/Debug/net10.0-windows/KTX-Management.AssemblyInfo.cs
+++ KTX-Management/obj/Debug/net10.0-windows/KTX-Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KTX-Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff2a9f8274079ee8ebd549258ee213917701a3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514f934a6ba815ec07f9964f939272f6130fa57f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KTX-Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KTX-Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
