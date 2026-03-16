664a0a806e6d9ac5bbd32588a1852b43ec4844f4
thelam06 <thelam10052006@gmail.com>
2026-03-16 11:50:14 +0700

    Fix error

--- KTX-Management/obj/Debug/net10.0-windows/KTX-Management.AssemblyInfo.cs
+++ KTX-Management/obj/Debug/net10.0-windows/KTX-Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KTX-Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7edd22f518f3611b333e84779d7d3069eb661b88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f06ab1ec2a26544a364ca361a423954f7b5a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("KTX-Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KTX-Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
